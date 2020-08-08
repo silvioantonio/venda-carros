c0b3a97cca6e9a8ce671ead18009b0ef2e244602
silvioantonio <oliveira_0000@hotmail.com>
2020-08-08 19:11:43 -0300

    Atualiza testes

--- Backend/src/test/java/com/silvio/plataforma/OpenSiteTest.java
+++ Backend/src/test/java/com/silvio/plataforma/OpenSiteTest.java
@@ -7,13 +7,16 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class OpenSiteTest {
 	
-	private String url = "http://localhost:4200/login";
+	private String url = "https://gestao-de-frotas.netlify.app";
 	private WebDriver driver;
 
 	@Before
@@ -30,14 +33,13 @@ public class OpenSiteTest {
 	@Test
 	public void testLoginUserFail() throws InterruptedException {
 		driver.get(this.url);
-		
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-		
-		driver.findElement(By.name("user")).sendKeys("teste");
-		driver.findElement(By.name("password")).sendKeys("senha123");
+
+		driver.findElement(By.id("usuario")).sendKeys("teste");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
 		driver.findElement(By.name("btnLogin")).click();
 		
-		assertNotEquals("http://localhost:4200/", driver.getCurrentUrl());
+		assertNotEquals("https://gestao-de-frotas.netlify.app/", driver.getCurrentUrl());
 		
 		Thread.sleep(5000);
 	}
@@ -47,31 +49,122 @@ public class OpenSiteTest {
 		driver.get(this.url);
 		
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-		
-		driver.findElement(By.name("user")).sendKeys("admin");
-		driver.findElement(By.name("password")).sendKeys("senha123");
+		driver.findElement(By.id("usuario")).sendKeys("admin");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
 		driver.findElement(By.name("btnLogin")).click();
 		
-		assertEquals("http://localhost:4200/", driver.getCurrentUrl());
+		assertEquals("https://gestao-de-frotas.netlify.app/", driver.getCurrentUrl());
 		
 		Thread.sleep(5000);
 	}
 
 	@Test
-	public void testVeiculos() throws InterruptedException {
+	public void testVeiculosRead() throws InterruptedException {
 		driver.get(this.url);
 		
 		driver.manage().timeouts().implicitlyWait(8000, TimeUnit.MILLISECONDS);
-		
-		driver.findElement(By.name("user")).sendKeys("admin");
-		driver.findElement(By.name("password")).sendKeys("senha123");
+		driver.findElement(By.id("usuario")).sendKeys("admin");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
 		driver.findElement(By.name("btnLogin")).click();
 		
-		driver.findElement(By.name("btnVeiculos")).click();
+		driver.findElement(By.linkText("Condutor")).click();
 		
-		assertEquals("http://localhost:4200/veiculos", driver.getCurrentUrl());
+		assertEquals("https://gestao-de-frotas.netlify.app/condutor", driver.getCurrentUrl());
 		
 		Thread.sleep(15000);
+	}
+	
+	@Test
+	public void testCondutorCreate() throws InterruptedException {
+		
+		driver.get(this.url);
+		
+		driver.findElement(By.id("usuario")).sendKeys("admin");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
+		driver.findElement(By.name("btnLogin")).click();		
+		driver.findElement(By.linkText("Condutor")).click();
+		
+		driver.findElement(By.id("btnNovo")).click();
+		driver.findElement(By.name("nome")).sendKeys("Teste Selenium");
+		driver.findElement(By.name("cpf")).sendKeys("12345678901");
+		driver.findElement(By.name("matricula")).sendKeys("1234");
+		driver.findElement(By.name("numeroCNH")).sendKeys("12456789");
+		driver.findElement(By.name("validade")).sendKeys("10102020");
+		driver.findElement(By.name("categoriaCnh")).click();
+		driver.findElement(By.id("D")).click();
+		driver.findElement(By.name("cidade")).sendKeys("Palmas");
+		driver.findElement(By.name("bairro")).sendKeys("Sul");
+		driver.findElement(By.name("complemento")).sendKeys("s/n");
+		driver.findElement(By.name("numero")).sendKeys("1");
+		driver.findElement(By.id("btnCriar")).click();
+		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.urlToBe("https://gestao-de-frotas.netlify.app/condutor"));
+		
+		driver.manage().timeouts().implicitlyWait(40000, TimeUnit.MILLISECONDS);
+		assertEquals("https://gestao-de-frotas.netlify.app/condutor", driver.getCurrentUrl());
+	}
+	
+	@Test
+	public void testCondutorUpdate() throws InterruptedException {
+		
+		driver.get(this.url);
+		
+		driver.findElement(By.id("usuario")).sendKeys("admin");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
+		driver.findElement(By.name("btnLogin")).click();		
+		driver.findElement(By.linkText("Condutor")).click();
+
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+		
+		driver.findElement(By.id("editar")).click();
+		
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+		
+		driver.findElement(By.name("cpf")).clear();
+		driver.findElement(By.name("cpf")).sendKeys("000011112345");
+		driver.findElement(By.name("matricula")).clear();
+		driver.findElement(By.name("matricula")).sendKeys("9876");
+		driver.findElement(By.name("cnh")).clear();
+		driver.findElement(By.name("cnh")).sendKeys("654321987");
+		driver.findElement(By.name("validade")).clear();
+		driver.findElement(By.name("validade")).sendKeys("25-08-2025");
+		driver.findElement(By.name("categoria")).click();
+		driver.findElement(By.id("E")).click();
+		driver.findElement(By.name("cidade")).clear();
+		driver.findElement(By.name("cidade")).sendKeys("Palmas");
+		driver.findElement(By.name("bairro")).clear();
+		driver.findElement(By.name("bairro")).sendKeys("Centro");
+		driver.findElement(By.name("complemento")).clear();
+		driver.findElement(By.name("complemento")).sendKeys("Casa 3");
+		driver.findElement(By.name("numero")).clear();
+		driver.findElement(By.name("numero")).sendKeys("0");
+		driver.findElement(By.id("btnSalvar")).click();
+		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.urlToBe("https://gestao-de-frotas.netlify.app/condutor"));
+		
+		driver.manage().timeouts().implicitlyWait(20000, TimeUnit.MILLISECONDS);
+		assertEquals("9876", driver.findElement(By.id("matricula")).getText());
+	}
+	
+	@Test
+	public void testCondutorDelete() throws InterruptedException {
+		
+		driver.get(this.url);
+		
+		driver.findElement(By.id("usuario")).sendKeys("admin");
+		driver.findElement(By.id("senha")).sendKeys("senha123");
+		driver.findElement(By.name("btnLogin")).click();		
+		driver.findElement(By.linkText("Condutor")).click();
+
+		driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+		
+		driver.findElement(By.id("deletar")).click();
+				
+		driver.findElement(By.name("btnDeletar")).click();
+		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.urlToBe("https://gestao-de-frotas.netlify.app/condutor"));
+		
+		assertEquals("https://gestao-de-frotas.netlify.app/condutor", driver.getCurrentUrl());
 	}
 
 }
